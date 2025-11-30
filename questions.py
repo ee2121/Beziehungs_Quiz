7bb48a4a7d1c047782d2c770ae1ee7961f846e7f
ee2121 <59110136+ee2121@users.noreply.github.com>
2025-12-01 00:47:39 +0100

    Update image paths in quiz data

--- questions.py
+++ questions.py
@@ -8,31 +8,31 @@
 
 quiz_data = [
     {
-        "image_path": "assets/q1.png",
+        "image_path": "q1.png",
         "question": "Wo war unser erster Kuss?",
         "options": ["In Düsseldorf", "In deiner Wohnung", "Im Plattenladen", "In Gangelt auf der Straße"],
         "correct_answer": "In Gangelt auf der Straße"
     },
     {
-        "image_path": "assets/q2.png",
+        "image_path": "q2.png",
         "question": "Wieviele Stadtausflüge haben wir schon gemeinsam gemacht?",
         "options": ["2", "4", "noch zu wenig", "10"],
         "correct_answer": "noch zu wenig"
     },
     {
-        "image_path": "assets/q3.png",
+        "image_path": "q3.png",
         "question": "Wo haben wir uns kennen gelernt?",
         "options": ["Auf ner Dating-Plattform", "Auf ner Party von nem gemeinsamen Freund", "Beim Einkaufen", "Beim Musikabend"],
         "correct_answer": "Auf ner Party von nem gemeinsamen Freund"
     },
     {
-        "image_path": "assets/q4.png",
+        "image_path": "q4.png",
         "question": "Wieviel Tage ist es her, als ich dich zum ersten Mal gefragt habe, ob du meine Freundin sein möchtest?",
         "options": ["184", "210", "214", "220"],
         "correct_answer": "214"
     },
     {
-        "image_path": "assets/q5.png",
+        "image_path": "q5.png",
         "question": "Wer ist mein absoluter Lieblingsmensch?",
         "options": ["Mein Squishy", "Kanye West", "Adam Sandler", "Max Verstappen"],
         "correct_answer": "Mein Squishy"
